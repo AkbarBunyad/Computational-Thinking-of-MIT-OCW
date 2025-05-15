5f26354f25d2f86b0e3f89608ac981b0fa954f17
Akbar <akbar.bunyadzade.std@bhos.edu.az>
2025-05-15 14:53:10 +0400

    fixed that tiny problem to get accurate range of pi

--- pi_value_with_Monte_Carlo_Simulation.py
+++ pi_value_with_Monte_Carlo_Simulation.py
@@ -4,9 +4,9 @@ import numpy as np
 def throw_needles(needle_num):
     in_circle = 0
     for n in range(1, needle_num+1):
-        x = random.random()
+        x = random.random() 
         y = random.random()
-        if (x**2 + y**22) ** 0.5 <= 1.0:
+        if (x**2 + y**2) ** 0.5 <= 1.0:
             in_circle+=1
     return 4*(in_circle / float(needle_num))
 
@@ -24,11 +24,11 @@ def get_estimation(num_needle, num_trials):
 def pi_estimation(precision, num_trials):
     needle_num = 1000
     std_dev = precision
-    while precision <= 2 * std_dev:
+    while precision <= 2*std_dev:
         estimation, std_dev = get_estimation(needle_num, num_trials)
         needle_num *= 2
     return estimation
 
-random.seed(42)
+random.seed(0)
 pi_estimation(0.005, 100)
 
